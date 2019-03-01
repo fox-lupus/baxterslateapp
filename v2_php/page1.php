2387861e2b6b253c3d3b1c1f826e6f37425d8b4d
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-03-01 14:07:54 -0500

    php and css stuff (3/1/2019)

---
+++ v2_php/page1.php
@@ -0,0 +1,9 @@
+<?php include('toppage.php'); ?>
+	<div class="header">
+		UPCOMING EVENTS
+	</div>
+
+	<div class="mainContent">
+		<p>No upcoming events scheduled.</p>
+
+	</div>
---
+++ v2_php/toppage.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" type="text/css" href="haeder.css">
+	<link href="https://fonts.googleapis.com/css?family=EB+Garamond|Zilla+Slab" rel="stylesheet"> 
+	<link rel="stylesheet" type="text/css" href="curtistuff.css">
+</head>
+<body>
+<div class="nav">
+	<div class="menu ">
+		<a href="" class="menu-text">menu</a>
+		<div class="dropdown_menu">
+			<a href="">LINK 1</a>
+			<a href="">LINK 2</a>
+			<a href="">LINK 3</a>
+			<a href="">LINK 4</a>
+		</div>
+	</div>
+	<div class="search">
+		<form>
+			<input type="text" placeholder="search...">
+		</form>
+	</div>
+</div>
