3ce1ebe89b7efd301ffed9589329dfdf14de5130
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-03-15 10:42:08 -0400

    new (3/15/19)

--- v2_php/competencydashboard.php
+++ v2_php/competencydashboard.php
@@ -1,24 +1,35 @@
 <?php include('header.php'); ?>	
-	
+<?php include('dots.php'); ?>
+
 	<div class="header">
 
 	</div>
 
 	<div class="mainContent">
-
-		<p>My Progress</p>
-
-		<div class="border"></div>				
-				
-		<p>Rubric Select:</p>
-
-		<div class="border"></div>
-
-		<p>HOW.1.0 Habits of Work</p>
-
-		<span style="background:#ff00fb;">Working Toward: Entering</span> 
 		
-		<p>Preparedness</p>
+		<div class="portfolios">
+		<div>Portfolios:</div>
+	
+	<div class="portfolio-colors">
+		<span style="background:#ff00fb;">EN</span> 
+		<span style="background:#ffa200;">PR</span> 
+		<span style="background:#21d130;">GB</span>
+		<span style="background:#21a5d1;">AD</span>
+		<span style="background:#8b21d1;">EX</span>  
+		<span style="background:#213fd1;">BA</span>    
+	</div>
+
+		<div class="Standards">
+			<div>
+			<span style="background:#a6a297;">HOW.1.0 Habits of Work</span>
+			</div>
+			<span style="background:#ff00fb;">Working Toward: Entering</span> 	
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	
+		<span style="background:#f5f3f0;"><!----<p>Preparedness</p>
 
 		<p>Box 1</p> 	<p>Box 2</p> 	<p>Box 3</p>
 		
@@ -28,20 +39,9 @@
 		
 		<p>Work Completion</p>
 
-		<p>Box 1</p> 	<p>Box 2</p> 	<p>Box 3</p>
+		<p>Box 1</p> 	<p>Box 2</p> 	<p>Box 3</p>----></span>
 
-<div class="portfolios">
-		<div>Portfolios:</div>
-	<div class="portfolio-colors">
-		<span style="background:#ff00fb;">EN</span> 
-		<span style="background:#ffa200;">PR</span> 
-		<span style="background:#21d130;">GB</span>
-		<span style="background:#21a5d1;">AD</span>
-		<span style="background:#8b21d1;">EX</span>  
-		<span style="background:#213fd1;">BA</span>    
-	</div>
-</div>	
-		<div class="border"></div>	
+<div class="border"></div>	
 					
 		</div>
 
