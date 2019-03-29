1dbc8b9fa9fbea2d8dead8a6b541ea258a1efe2c
Rob Korobkin <rob.korobkin@gmail.com>
2019-03-29 14:01:05 -0400

    Help with page architecture.

--- v2_php/taskdashboard.php
+++ v2_php/taskdashboard.php
@@ -3,18 +3,19 @@
 <div class="dot-baner">
 	<div class="spaser">
 		<div class ="contactusmainfont">
-		<span class="My-Tasks">My Tasks</span>
-		<div class="select-block">
-			<span class="Course-Selection">Course Selection</span>
-			<select class="selection-box">	
-				<option value="">All</option>
-				<option value="">1</option>
-				<option value="">2</option>
-				<option value="">3</option>
-				<option value="">4</option>
-				<option value="">5</option>
-				<option value="">6</option>
-			</select>
+			<span class="My-Tasks">My Tasks</span>
+			<div class="select-block">
+				<span class="Course-Selection">Course Selection</span>
+				<select class="selection-box">	
+					<option value="">All</option>
+					<option value="">1</option>
+					<option value="">2</option>
+					<option value="">3</option>
+					<option value="">4</option>
+					<option value="">5</option>
+					<option value="">6</option>
+				</select>
+			</div>
 		</div>
 	</div>	
 </div>
@@ -23,42 +24,56 @@
 
 	<div class="taskdashboard">
 
-		<div class="headline">
-			<span class="courseNum">Current Tasks</span>
-				<div class="spaser">
-					<div class ="contactusmainfont">
-					<div class="select-block">
-						<select class="selection-box">
-							<option value="">Filter</option>	
-							<option value="">Due Tasks</option>
-							<option value="">Revision Tasks</option>
-							<option value="">Submitted Tasks</option>
-							<option value="">Completed Tasks</option>
+
+		<!-- PANEL ONE -->
+		<div class="container left">
+
+			<div class="headline">
+				
+				<span class="courseNum">Current Tasks</span>
+
+				<div class="select-block">
+					<select class="selection-box">
+						<option value="">Filter</option>	
+						<option value="">Due Tasks</option>
+						<option value="">Revision Tasks</option>
+						<option value="">Submitted Tasks</option>
+						<option value="">Completed Tasks</option>
 					</select>
 				</div>
-		</div>	
-</div>
+				
+			</div>
 
-		</div>
-		<div class="subhead2">
-			<div>No tasks found</div>
-		</div>
+			<div class="subhead2">
+				<div>No tasks found</div>
+			</div>
 		
-		<div class="headline">
-			<span class="courseNum">To-Do List</span>
-			<span class="courseName">Personal</span>
 		</div>
 
 
-		<span class="contactustinyfont">
-		<div class="subhead">Active Items</div>
+		<!-- PANEL TWO -->
+		<div class="container right">
 
-		<div class="content">
-			<div class="creditsFrame">
-			<div>New to-do...</div>
-			<div>mm/dd/yyyy
-				<div class="credit"></div>
+			<div class="headline">
+				<span class="courseNum">To-Do List</span>
+				<span class="courseName">Personal</span>
+			</div>
 
+			<div class="subhead contactustinyfont">Active Items</div>
+
+			<div class="content">
+				<div class="creditsFrame">
+					<div>New to-do...</div>
+					<div>mm/dd/yyyy</div>
+					<div class="credit"></div>
+				</div>
+			</div>
+		</div>
+
+
+		<div style="clear: both"></div>
+
+	</div>
 </div>
 
 	
