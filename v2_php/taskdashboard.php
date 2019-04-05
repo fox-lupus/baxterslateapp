fcf812ea6751d034f085d70ec4c3d44f89cd1e69
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-04-05 12:52:46 -0400

    .

--- v2_php/taskdashboard.php
+++ v2_php/taskdashboard.php
@@ -14,29 +14,27 @@
 				<span class="courseNum">Current Tasks</span>
 
 				<div class="select-block">
-					<select class="selection-box">
-						<option value="">Filter</option>
-						<option value="">STATUS</option>
-						<option value="">Due Tasks</option>
-						<option value="">Revision Tasks</option>
-						<option value="">Submitted Tasks</option>
-						<option value="">Completed Tasks</option>
-						<option value="">TIMELINE</option>
-						<option value="">Past Due</option>
-						<option value="">Due Today</option>
-						<option value="">Due This Week</option>
-						<option value="">Due Next Week</option>
-					</select>
-				</div>
-				
-			</div>
-
+							<select class="selection-box">
+								<option value="">Filter</option>
+								<option value="">STATUS</option>
+								<option value="">Due Tasks</option>
+								<option value="">Revision Tasks</option>
+								<option value="">Submitted Tasks</option>
+								<option value="">Completed Tasks</option>
+								<option value="">TIMELINE</option>
+								<option value="">Past Due</option>
+								<option value="">Due Today</option>
+								<option value="">Due This Week</option>
+								<option value="">Due Next Week</option>
+							</select>
+						</div>
+					</div>
+			
 			<div class="subhead2">
 				<div>No tasks found</div>
 			</div>
 		
 		</div>
-
 
 		<!-- PANEL TWO -->
 		<div class="container right">
