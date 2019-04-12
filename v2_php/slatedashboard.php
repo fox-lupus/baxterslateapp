1bc2882a12923192f173b0574c32d008755fb5cc
Rob Korobkin <rob.korobkin@gmail.com>
2019-04-12 13:36:52 -0400

    Formatting instruction.

--- v2_php/slatedashboard.php
+++ v2_php/slatedashboard.php
@@ -2,114 +2,105 @@
 
 <div class="mainContent slatedashboard">
 
-	<div class="pageTitle">My Dashboard</p>
+	<div class="pageTitle">My Dashboard</div>
 
-	<div class="section">
-		<div class="title">Calendar</div>
-		<div class="body">No future events scheduled</div>
-	</div>
+	<div class="container_left">
 
-	<div class="section">
-		<div class="title">Courses</div>
+		<div class="section">
+			<div class="title">Courses</div>
 
-		<div class="iconFrame">
-			
-			<a href="https://baxter.slatepowered.net/sections/spring1819-ADV009-1" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Advisory 9
+			<div class="iconFrame">
+				
+				<a href="https://baxter.slatepowered.net/sections/spring1819-ADV009-1" class="dashboardLink">
+					
+					<div class="default_icon"></div>
+
+					Advisory 9
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-FLX900-304" class="dashboardLink">
+					<div class="default_icon"></div>
+					Flex Friday Project
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-SCI250-3" class="dashboardLink">
+					<div class="default_icon"></div>
+					Physical Science
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-MAT200-4" class="dashboardLink">
+					<div class="default_icon"></div>
+					Advanced Algebra
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-COS100-2" class="dashboardLink">
+					<div class="default_icon"></div>
+					Intro to Computer Science
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-ENG202-1" class="dashboardLink">
+					<div class="default_icon"></div>
+					Coming of Age
+				</a>
+
+				<a href="https://baxter.slatepowered.net/sections/spring1819-SPA350-1" class="dashboardLink">
+					<div class="default_icon"></div>
+					Spanish III/IV
+				</a>
+
+				<div style="clear: both"></div>
+				
+
+			</div>
+		</div>
+
+		<div class="section">
+			<div class="title">Tools</div>
+			<a href ="https://baxter.slatepowered.net/cbl/dashboards/demonstrations/student#me" class="dashboardLink">
+				<img src="img/2nd.png">
+				Competency Dashboard
 			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-FLX900-304" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Flex Friday Project
-			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-SCI250-3" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Physical Science
-			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-MAT200-4" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Advanced Algebra
-			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-COS100-2" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Intro to Computer Science
-			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-ENG202-1" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Coming of Age
-			</a>
-
-			<a href="https://baxter.slatepowered.net/sections/spring1819-SPA350-1" class="dashboardLink">
-				<svg width="100" height="100">
-  				<rect rx="25" ry="25" width="90" height="90"
-  				style="fill:#31A0DA;stroke:#31A0DA;stroke-width:0.1;"/>
-				</svg>
-				Spanish III/IV
+			<a href ="https://baxter.slatepowered.net/cbl/dashboards/tasks/student#me/all" class="dashboardLink">
+				<img src="img/2nd.png">
+				Task Dashboard
 			</a>
 
 			<div style="clear: both"></div>
-			
 
+			<div class="body"></div>
+		</div>
+
+		<div class="section">
+			<div class="title">Name</div>
+			<a href ="https://baxter.slatepowered.net/people/curtisruel" class="dashboardLink">
+				<img src="img/3rd.png">
+				My Profile
+			</a>
+			<a href ="https://baxter.slatepowered.net/profile" class="dashboardLink">
+				<img src="img/4th.png">
+				Edit Profile
+			</a>
+			<a href ="https://baxter.slatepowered.net/drafts" class="dashboardLink">
+				<img src="img/5th.png">
+				My Drafts
+			</a>
+			<a href ="https://baxter.slatepowered.net/dashboard" class="dashboardLink">
+				<img src="img/5th.png">
+				Log Out
+			</a>
+
+			<div style="clear: both"></div>
+
+			<div class="body"></div>
+			
 		</div>
 	</div>
 
-	<div class="section">
-		<div class="title">Tools</div>
-		<a href ="https://baxter.slatepowered.net/cbl/dashboards/demonstrations/student#me" class="dashboardLink">
-			<img src="img/2nd.png">
-			Competency Dashboard
-		<a href ="https://baxter.slatepowered.net/cbl/dashboards/tasks/student#me/all" class="dashboardLink">
-			<img src="img/2nd.png">
-			Task Dashboard
-		</a>
-
-		<div style="clear: both"></div>
-
-		<div class="body"></div>
+	<div class="container_right">
+		<div class="section">
+			<div class="title">Calendar</div>
+			<div class="body">No future events scheduled</div>
+		</div>
 	</div>
 
-	<div class="section">
-		<div class="title">Name</div>
-		<a href ="https://baxter.slatepowered.net/people/curtisruel" class="dashboardLink">
-			<img src="img/3rd.png">
-			My Profile
-		<a href ="https://baxter.slatepowered.net/profile" class="dashboardLink">
-			<img src="img/4th.png">
-			Edit Profile
-		<a href ="https://baxter.slatepowered.net/drafts" class="dashboardLink">
-			<img src="img/5th.png">
-			My Drafts
-		<a href ="https://baxter.slatepowered.net/dashboard" class="dashboardLink">
-			<img src="img/5th.png">
-			Log Out
-		</a>
-
-		<div style="clear: both"></div>
-
-		<div class="body"></div>
-		
-	</div>
 </div>
