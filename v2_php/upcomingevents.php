e5f354c71fe852577fbde67d11f6a671367aeca1
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-04-26 09:05:49 -0400

    .

--- v2_php/upcomingevents.php
+++ v2_php/upcomingevents.php
@@ -3,18 +3,11 @@
 <div class="mainContent">
 		<div class="spaser">
 			<div class="upcomingeventsmainfont">
-			<span class="Upcoming-Events">Upcoming Events</span>
+			<span class="Calendar">Calendar</span>
 		</div>
 	
 	<div class="iphoneFrame">
 
 		</div>
 
-		<div class="footer">
-
-			<div class="menu"></div>
-
-			<p>Slate is an open-source web platform for schools. Learn more.</p>
-
-	</div>
 <?php include ('otherphp/footer.php'); ?>
