77e5c4757b3a1249e37b68711f5401f794fc64a5
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-05-10 09:16:24 -0400

    the first of many changes (5/10/2019)

--- v2_php/slatedashboard.php
+++ v2_php/slatedashboard.php
@@ -32,9 +32,9 @@
 					<div class="body">
 						<div>spring1819-ADV009-1</div>
 						<div>TERM	spring1819</div>
-						<div>SCHEDULE	Advisory</div>
+						<div>SCHEDULE:	Advisory</div>
 						<div>LOCATION</div>
-						<div>Teacher: John Appleseed</div>
+						<div>Teacher: Sarah Buhlman</div>
 						<div>Students:</div> 
 						<div>Student 1, Student 2, Student 3</div>
 						<div>Student 4, Student 5, Student 6,</div>
@@ -46,11 +46,137 @@
 						More Information
 					</a>
 				</div>
+			</div>
 
 				<div class="item">
 					<div class="title">Flex Friday Project</div>
-					<div class="body">I AM THE INFO ABOUT FLEX FRIDAY</div>
+					<div class="body">
+						<div>spring1819-FLX900-304</div>
+						<div>TERM	spring1819</div>
+						<div>SCHEDULE:	All Day (Friday's Only)</div>
+						<div>LOCATION</div>
+						<div>Teachers: Teacher 1, Teacher 2, Teacher 3</div>
+						<div>Students:</div>
+						<div>Student 1, Student 2, Student 3</div>
+						<div>Student 4, Student 5, Student 6,</div>
+						<div>Student 7, Student 8, Student 9,</div>
+						<div>Student 10 Student 11, Student 12,</div>
+						<div>Student 13 Student 14, Student 15</div>
+					<a href="https://baxter.slatepowered.net/sections/spring1819-FLX900-304" class="dashboardlink">
+
+						More Information
+					</a>
 				</div>
+			</div>
+			
+				<div class="item">
+					<div class="title">Physical Science</div>
+					<div class="body">
+					<div>spring1819-SCI250-3</div>
+					<div>TERM	spring1819</div>
+					<div>SCHEDULE: Block C</div>
+					<div>LOCATION</div>
+					<div>Teacher: Jonathan Doughty</div>
+					<div>Students:</div>
+					<div>Student 1, Student 2, Student 3</div>
+					<div>Student 4, Student 5, Student 6,</div>
+					<div>Student 7, Student 8, Student 9,</div>
+					<div>Student 10 Student 11, Student 12,</div>
+					<div>Student 13 Student 14, Student 15</div>
+				<a href="https://baxter.slatepowered.net/sections/spring1819-SCI250-3" class="dashboardlink">
+
+					    More Information
+				    </a>
+			    </div>
+
+			    <div class="item">
+					<div class="title">Advanced Algebra</div>
+					<div class="body">
+					<div>spring1819-MAT200-4</div>
+					<div>TERM	spring1819</div>
+					<div>SCHEDULE: Block A</div>
+					<div>LOCATION</div>
+					<div>Teacher: Julia Kosowsky</div>
+					<div>Students:</div>
+					<div>Student 1, Student 2, Student 3</div>
+					<div>Student 4, Student 5, Student 6,</div>
+					<div>Student 7, Student 8, Student 9,</div>
+					<div>Student 10 Student 11, Student 12,</div>
+					<div>Student 13 Student 14, Student 15</div>
+				<a href="https://baxter.slatepowered.net/sections/spring1819-MAT200-4" class="dashboardlink">
+
+					    More Information
+				    </a>
+			    </div>
+			</div>
+
+			    <div class="item">
+					<div class="title">Intro to Computer Science</div>
+					<div class="body">
+					<div>spring1819-COS100-2</div>
+					<div>TERM	spring1819</div>
+					<div>SCHEDULE: Block B</div>
+					<div>LOCATION</div>
+					<div>Teacher: Pamela Rawson</div>
+					<div>Students:</div>
+					<div>Student 1, Student 2, Student 3</div>
+					<div>Student 4, Student 5, Student 6,</div>
+					<div>Student 7, Student 8, Student 9,</div>
+					<div>Student 10 Student 11, Student 12,</div>
+					<div>Student 13 Student 14, Student 15</div>
+				<a href="https://baxter.slatepowered.net/sections/spring1819-COS100-2" class="dashboardlink">
+
+					    More Information
+				    </a>
+			    </div>
+			</div>
+
+			 <div class="item">
+					<div class="title">Coming Of Age</div>
+					<div class="body">
+					<div>spring1819-ENG202-1</div>
+					<div>TERM	spring1819</div>
+					<div>SCHEDULE: Block E</div>
+					<div>LOCATION</div>
+					<div>Teacher: Breanne Lucy</div>
+					<div>Students:</div>
+					<div>Student 1, Student 2, Student 3</div>
+					<div>Student 4, Student 5, Student 6,</div>
+					<div>Student 7, Student 8, Student 9,</div>
+					<div>Student 10 Student 11, Student 12,</div>
+					<div>Student 13 Student 14, Student 15</div>
+				<a href="https://baxter.slatepowered.net/sections/spring1819-ENG202-1" class="dashboardlink">
+
+					    More Information
+				    </a>
+			    </div>
+			</div>
+
+			 <div class="item">
+					<div class="title">Spanish III/IV</div>
+					<div class="body">
+					<div>spring1819-SPA350-1</div>
+					<div>TERM	spring1819</div>
+					<div>SCHEDULE: Block D</div>
+					<div>LOCATION</div>
+					<div>Teacher: Megan Lawrence</div>
+					<div>Students:</div>
+					<div>Student 1, Student 2, Student 3</div>
+					<div>Student 4, Student 5, Student 6,</div>
+					<div>Student 7, Student 8, Student 9,</div>
+					<div>Student 10 Student 11, Student 12,</div>
+					<div>Student 13 Student 14, Student 15</div>
+				<a href="https://baxter.slatepowered.net/sections/spring1819-SPA350-1" class="dashboardlink">
+
+					    More Information
+				    </a>
+			    </div>
+			</div>
+
+
+
+
+
 		
 				<!-- 
 					<div class="body">
