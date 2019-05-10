ed73242d59692e7a7fc614c49c18475d5061d94a
Rob Korobkin <rob.korobkin@gmail.com>
2019-05-10 13:24:18 -0400

    Help with task dashboard.

--- v2_php/competencydashboard.php
+++ v2_php/competencydashboard.php
@@ -1,167 +1,138 @@
 <?php include('otherphp/header.php'); ?>	
 <?php include('otherphp/dots.php'); ?>
 	
-	<div class="mainContent">
-	<div>
-	
-	</div>
-		<div class="portfolios">
+<div class="mainContent">
+
+	<div class="portfolios">
+
 		<div>Portfolios:</div>
-	<div class="portfolio-colors">
-		<div style="background:#ff00fb;">EN</div> 
-		<div style="background:#ffa200;">PR</div> 
-		<div style="background:#21d130;">GB</div>
-		<div style="background:#21a5d1;">AD</div>
-		<div style="background:#8b21d1;">EX</div>  
-		<div style="background:#213fd1;">BA</div>
-		<div style="clear: both; float: none;"></div>a
-	</div>
 
-	<div class="standard_container">
-
-		<div class="headline">
-			<span class="courseNum">HOW.1.0</span>
-			<span class="courseName">Habits of Work</span>
+		<div class="portfolio-colors">
+			<div class="en">EN</div> 
+			<div class="pr">PR</div> 
+			<div class="gb">GB</div>
+			<div class="ad">AD</div>
+			<div class="ex">EX</div>  
+			<div class="ba">BA</div>
+			<div style="clear: both; float: none;"></div>a
 		</div>
 
-		<div class="subhead">Working Toward: Entering</div>
+		<div class="standard_container">
+
+			<div class="headline">
+				<span class="courseNum">HOW.1.0</span>
+				<span class="courseName">Habits of Work</span>
+			</div>
+
+			<div class="subhead en">Working Toward: Entering</div>
+			
+			<div class="content">
+				Preparedness
+				<div class="creditsFrame">
+					<div class="credit en">EN</div>
+					<div class="credit pr">PR</div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+			<div class="content">
+				Engagement
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+			<div class="content">
+				Work Completion
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+		</div>
+
+
+		<div class="standard_container">
+			<div class="headline">
+				<span class="courseNum">HUM.2.0.0</span>
+				<span class="courseName">Reading</span>
+			</div>
+
+			<div class="subhead pr">Working Toward: Progressing</div>
+			
+			<div class="content">
+				Craft and Structure
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+			<div class="content">
+				Key Ideas and Details
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+			<div class="content">
+				Citing Evidence
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+		</div>
+
 		
-		<div class="content">
-			Preparedness
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
+		<div class="standard_container">
 
-		<div class="content">
-			Engagement
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
+			<div class="headline">
+				<span class="courseNum">DES.1.0.0</span>
+				<span class="courseName">Design Process</span>
 			</div>
-		</div>
 
-		<div class="content">
-			Work Completion
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
+			<div class="subhead gb">Working Toward: Graduation Benchmark</div>
+			
+			<div class="content">
+				Analyze and optimize
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
 			</div>
-		</div>
 
-		<div class="border"></div>	
-					
+			<div class="content">
+				Develop Solution
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+			<div class="content">
+				Identify Problems
+				<div class="creditsFrame">
+					<div class="credit"></div>
+					<div class="credit"></div>
+					<div class="credit"></div>
+				</div>
+			</div>
+
+		</div>
 	</div>
+</div>
 
-	<div>
-
-		<div class="footer">
-		<div class="menu"></div>
 	
-		<div class="border"></div>	
-
-	</div>
-
-	<div class="standard_container">
-
-		<div class="headline">
-			<span class="courseNum">HUM.2.0.0</span>
-			<span class="courseName">Reading</span>
-		</div>
-
-		<div class="subhead3">Working Toward: Progressing</div>
-		
-		<div class="content">
-			Craft and Structure
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="content">
-			Key Ideas and Details
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="content">
-			Citing Evidence
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="border"></div>	
-					
-	</div>
-
-	<div>
-
-		<div class="footer">
-		<div class="menu"></div>
-	
-		<div class="border"></div>	
-
-	</div>
-
-	<div class="standard_container">
-
-		<div class="headline">
-			<span class="courseNum">DES.1.0.0</span>
-			<span class="courseName">Design Process</span>
-		</div>
-
-		<div class="subhead2">Working Toward: Graduation Benchmark</div>
-		
-		<div class="content">
-			Analyze and optimize
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="content">
-			Develop Solution
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="content">
-			Identify Problems
-			<div class="creditsFrame">
-				<div class="credit"></div>
-				<div class="credit"></div>
-				<div class="credit"></div>
-			</div>
-		</div>
-
-		<div class="border"></div>	
-					
-	</div>
-
-	<div>
-
-		<div class="footer">
-		<div class="menu"></div>
-	
-		<div class="border"></div>	
-
-	</div>
-
 <?php include('otherphp/footer.php'); ?>	
