5f7783765c20382ef71e08fced1b3b66e108b191
fox-lupus <42156458+fox-lupus@users.noreply.github.com>
2019-05-29 12:40:55 -0400

    -w

--- v2_php/contactus.php
+++ v2_php/contactus.php
@@ -1,39 +1,24 @@
 <?php include('otherphp/header.php'); ?>	
-
-	<div class="header">
-
+<div class="modge-podge">
+	<div class ="contactusmainfont">
+		<div>Contact Us</div>
 	</div>
-	
-	<div class="mainContent">
-	<div>
-		<div class ="contactusmainfont">
-			<div>Contact Us</div>
-		</div>
-<div class="contactustinyfont">
-<div class="boxcontactus">
-</div>
-<p>NAME*:</p>
-	<input class="textInput"/> 
-	<p>EMAIL*:</p>
+	<div class="contactustinyfont">
+		<div class="boxcontactus"></div>
+		<p>NAME*:</p>
+		<input class="textInput"/> 
+		<p>EMAIL*:</p>
 		<input class="textInput"/>
-	<p>PHONE:</p>
+		<p>PHONE:</p>
 		<input class="textInput"/>
-	<p>Optional. Include your area code.</p>
-	<p>MESSAGE*:</p>
+		<p>Optional. Include your area code.</p>
+		<p>MESSAGE*:</p>
 		<input class="textInput2"/>
-	<p>*Required</p>
-	<div class="sendbutton"/>
-	<p>Send</p>
-	</div>
-	
-	<div class="iphoneFrame">
-					
-	</div>
-
+		<p>*Required</p>
+		<div class="sendbutton"/>
+		<p>Send</p>
 		</div>
-
-			<div class="menu"></div>
-
 	</div>
+</div>
 
 <?php include('otherphp/footer.php'); ?>	
