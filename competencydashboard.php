2f95e31f0c3fa455cdd1f94e22d7c501c42070f4
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-05-31 11:16:27 -0400

    Create competencydashboard.php

---
+++ competencydashboard.php
@@ -0,0 +1,142 @@
+<?php include('otherphp/header.php'); ?>	
+<?php include('otherphp/dots.php'); ?>
+	
+<div class="mainContent">
+
+	<div class="portfolios">
+
+		<div class="portfolio-border">
+			
+			<div>Portfolios:</div>
+
+			<div class="portfolio-colors">
+				<div class="en portfolio">EN</div> 
+				<div class="pr portfolio">PR</div> 
+				<div class="gb portfolio">GB</div>
+				<div class="ad portfolio">AD</div>
+				<div class="ex portfolio">EX</div>  
+				<div class="ba portfolio">BA</div>
+				<div style="clear: both; float: right; margin-right: 25px;"></div>a
+			</div>
+		</div>
+		
+		<div class="standard_container">
+
+			<div class="headline">
+				<span class="courseNum">HOW.1.0</span>
+				<span class="courseName">Habits of Work</span>
+			</div>
+
+			<div class="subhead en">Working Toward: Entering</div>
+			
+			<div class="content">
+				Preparedness
+				<div class="creditsFrame">
+					<div class="credit en">EN</div>
+					<div class="credit en">EN</div>
+					<div class="credit pr">PR</div><img src="img/checkmarken.png">
+
+				</div>
+			</div>
+
+			<div class="content">
+				Engagement
+				<div class="creditsFrame">
+					<div class="credit en">EN</div>
+					<div class="credit pr">PR</div>
+					<div class="credit pr">PR</div><img src="img/checkmarken.png">
+				</div>
+			</div>
+
+			<div class="content">
+				Work Completion
+				<div class="creditsFrame">
+					<div class="credit en">EN</div>
+					<div class="credit en">EN</div>
+					<div class="credit en">EN</div><img src="img/checkmarken.png">
+				</div>
+			</div>
+
+		</div>
+
+
+		<div class="standard_container">
+			<div class="headline">
+				<span class="courseNum">HUM.2.0.0</span>
+				<span class="courseName">Reading</span>
+			</div>
+
+			<div class="subhead pr">Working Toward: Progressing</div>
+			
+			<div class="content">
+				Craft and Structure
+				<div class="creditsFrame">
+					<div class="credit pr">PR</div>
+					<div class="credit pr">PR</div>
+					<div class="credit pr">PR</div><img src="img/checkmarkpr.png">
+				</div>
+			</div>
+
+			<div class="content">
+				Key Ideas and Details
+				<div class="creditsFrame">
+					<div class="credit en">EN</div>
+					<div class="credit pr">PR</div>
+					<div class="credit pr">PR</div><img src="img/checkmarken.png">
+				</div>
+			</div>
+
+			<div class="content">
+				Citing Evidence
+				<div class="creditsFrame">
+					<div class="credit pr">PR</div>
+					<div class="credit gb">GB</div>
+					<div class="credit gb">GB</div><img src="img/checkmarkpr.png">
+				</div>
+			</div>
+
+		</div>
+
+		
+		<div class="standard_container">
+
+			<div class="headline">
+				<span class="courseNum">DES.1.0.0</span>
+				<span class="courseName">Design Process</span>
+			</div>
+
+			<div class="subhead gb">Working Toward: Graduation Benchmark</div>
+			
+			<div class="content">
+				Analyze and optimize
+				<div class="creditsFrame">
+					<div class="credit gb">GB</div>
+					<div class="credit gb">GB</div>
+					<div class="credit gb">GB</div><img src="img/checkmarkgb.png">
+				</div>
+			</div>
+
+			<div class="content">
+				Develop Solution
+				<div class="creditsFrame">
+					<div class="credit pr">PR</div>
+					<div class="credit gb">GB</div>
+					<div class="credit gb">GB</div><img src="img/checkmarkpr.png">
+				</div>
+			</div>
+
+			<div class="content">
+				Identify Problems
+				<div class="creditsFrame">
+					<div class="credit gb">GB</div>
+					<div class="credit pr">PR</div>
+					<div class="credit pr">PR</div><img src="img/checkmarkpr.png">
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+	
+<?php include('otherphp/footer.php'); ?>	
