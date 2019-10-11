1ad99a0376e0fa1e4963c63e22fa271b78010b8b
fox-lupus <42156458+fox-lupus@users.noreply.github.com>
2019-10-11 08:52:06 -0400

    add footer
    add black background on bottom of footer

--- v2_php/otherphp/footer.php
+++ v2_php/otherphp/footer.php
@@ -1,8 +1,8 @@
 <div class="slate-thing">
-				<span>Slate is an open-source web platform for schools. <a href="">Learn more</a>.</span>
+				<span>Slate is an open-source web platform for schools. <a href="#top">Learn more</a>.</span>
 </div>
 <div class="bottombackground">
 	
 </div>	
 </body>
-</html>
+</html>S
