c71b25f8385ec2b96465bfc8149b4579df4b69f5
curtism1804 <44301817+curtism1804@users.noreply.github.com>
2019-11-01 09:11:19 -0400

    1st changes (11/1/2019)

--- v2_php/competencydashboard.php
+++ v2_php/competencydashboard.php
@@ -32,12 +32,11 @@
 				<div class="ad">AD</div>
 				<div class="ex">EX</div>  
 				<div class="ba">BA</div>
+					</div>
 
-
-				<div style="clear: both; float: right; margin-right: 25px;"></div>a
+				<div style="clear: both; float: right; margin-right: 25px;"></div>
 			</div>
-		</div>
-		
+		</div>	
 		<div class="standard_container">
 
 			<div class="headline">
